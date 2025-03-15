74d840438b3f42f58c6831c14826aef87e46db75
Nicholas Stull <Nicholasp.stull@gmail.com>
2025-03-15 15:19:06 -0400

    Initial Commit

---
+++ DisplayFlipper/DisplayFlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayFlipperApp.swift
+//  DisplayFlipper
+//
+//  Created by Nick Stull on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DisplayFlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisplayFlipperTests/DisplayFlipperTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayFlipperTests.swift
+//  DisplayFlipperTests
+//
+//  Created by Nick Stull on 3/15/25.
+//
+
+import Testing
+@testable import DisplayFlipper
+
+struct DisplayFlipperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
